8307ccca65ac082a4f02c14d35a243fbe12ca902
MariaFrem <58810165+MariaFrem@users.noreply.github.com>
2019-12-12 11:23:14 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Julekalender in cloud by Maria
         </p>
         <a
           className="App-link"
